7d7840bbcf8e7513cbfc9485a03541ef13a8a3e4
kndhvh <delend89@gmail.com>
2023-09-18 13:50:16 -0300

    feat: streamlit tabs

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -12,13 +12,13 @@ st.image(
     use_column_width="always",
 )
 
+tab1, tab2 = st.tabs(["Tinder Like Recomendation 🔥","Search Recomendation 🔎"])
 init_session_state()
 
 if st.session_state.current_movie is None:
     st.session_state.current_movie = MoviesApi.get_random_movie()
 
-
-if st.session_state.current_movie is not None:
+with tab1:
     _, c1, _ = st.columns((1, 0.8, 1))
     _, _, c2, c3, _, _ = st.columns(6)
 
@@ -39,18 +39,18 @@ if st.session_state.current_movie is not None:
             st.session_state.current_movie = st.session_state.recomendations[-1]
             st.experimental_rerun()
 
-# with c1: st.markdown(f"##### Blacklist Size: {len(st.session_state.blacklist)}")
 
-_, c1, _,= st.columns(3)
-with c1:
-    my_expander = st.expander("Selecione um filme 🎬", expanded=True)
-    selected_movie_name = my_expander.selectbox("", MoviesApi.get_all_movies())
+with tab2:
+    _, c1, _,= st.columns(3)
+    with c1:
+        my_expander = st.expander("Selecione um filme 🎬", expanded=True)
+        selected_movie_name = my_expander.selectbox("", MoviesApi.get_all_movies())
 
-if my_expander.button("Recomendar"):
-    st.write("#")
-    recomendations = MoviesApi.get_recomendation(selected_movie_name)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    cols = [col1, col2, col3, col4, col5]
-    for i in range(0, 5):
-        with cols[i]:
-            MovieWidgets.display_movie(recomendations[i])
+    if my_expander.button("Recomendar Similares"):
+        st.write("#")
+        recomendations = MoviesApi.get_recomendation(selected_movie_name)
+        col1, col2, col3, col4, col5 = st.columns(5)
+        cols = [col1, col2, col3, col4, col5]
+        for i in range(0, 5):
+            with cols[i]:
+                MovieWidgets.display_movie(recomendations[i])
