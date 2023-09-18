3ca167ec44cb14e07ef5947de935cc5d4d4ef88f
kndhvh <delend89@gmail.com>
2023-09-18 14:27:18 -0300

    feat: streamlit tabs

--- frontend/app/cache/session_state.py
+++ frontend/app/cache/session_state.py
@@ -1,9 +1,12 @@
+from service.movies_api import MoviesApi
 import streamlit as st
 
 def init_session_state():
     if 'current_movie' not in st.session_state:
-        st.session_state.current_movie = None
+        st.session_state.current_movie = MoviesApi.get_random_movie()
     if 'blacklist' not in st.session_state:
         st.session_state.blacklist = []
     if 'recomendations' not in st.session_state:
         st.session_state.recomendations = None
+    if 'movies' not in st.session_state:
+        st.session_state.movies = MoviesApi.get_all_movies()
--- frontend/app/main.py
+++ frontend/app/main.py
@@ -15,16 +15,13 @@ st.image(
 tab1, tab2 = st.tabs(["Tinder Like Recomendation 🔥","Search Recomendation 🔎"])
 init_session_state()
 
-if st.session_state.current_movie is None:
-    st.session_state.current_movie = MoviesApi.get_random_movie()
-
 with tab1:
     _, c1, _ = st.columns((1, 0.8, 1))
     _, _, c2, c3, _, _ = st.columns(6)
 
-    st.session_state.recomendations = MoviesApi.get_recomendation(
-        st.session_state.current_movie, st.session_state.blacklist
-    )
+    if st.session_state.recomendations is None:
+        st.session_state.recomendations = MoviesApi.get_recomendation(st.session_state.current_movie, st.session_state.blacklist)
+    
     with c1:
         MovieWidgets.display_movie(st.session_state.current_movie)
 
@@ -32,11 +29,13 @@ with tab1:
         if st.button("👍", use_container_width=True):
             st.session_state.blacklist.append(st.session_state.current_movie)
             st.session_state.current_movie = st.session_state.recomendations[0]
+            st.session_state.recomendations = None
             st.experimental_rerun()
     with c2:
         if st.button("👎", use_container_width=True):
             st.session_state.blacklist.append(st.session_state.current_movie)
             st.session_state.current_movie = st.session_state.recomendations[-1]
+            st.session_state.recomendations = None
             st.experimental_rerun()
 
 
@@ -44,7 +43,7 @@ with tab2:
     _, c1, _,= st.columns(3)
     with c1:
         my_expander = st.expander("Selecione um filme 🎬", expanded=True)
-        selected_movie_name = my_expander.selectbox("", MoviesApi.get_all_movies())
+        selected_movie_name = my_expander.selectbox("", st.session_state.movies)
 
     if my_expander.button("Recomendar Similares"):
         st.write("#")
